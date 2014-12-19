fc3f18a869c335484753483327eef329196b8249
amzotti <azotti1337@gmail.com>
2014-12-19 01:52:32 -0800

    Create wrapper for TimeWizard

--- TimeWizard.rb
+++ TimeWizard.rb
@@ -34,27 +34,3 @@ class TimeWizard
     commit_file name
   end
 end
-
-#ruby -r "./TimeWizard.rb" -e "runTimeWizard('Bool.java')"
-def runTimeWizard(file, days = "01", months = "09")
-
-  time = TimeWizard.new(
-    month = months.to_s,
-    day = days.to_s,
-    hours(),
-    minutes(),
-    seconds()
-  )
-
-  time.teleport_file("#{file}")
-end
-
-def minutes
-  (rand(58) + 1).to_s
-end
-
-alias :seconds :minutes
-
-def hours
-  (rand(22) + 1).to_s
-end
---
+++ TimeWizardWrapper.rb
@@ -0,0 +1,34 @@
+require './TimeWizard.rb'
+
+class TimeWizardWrapper
+
+  #Static methods are really instance methods of class's eigenclass
+  #We need to do this so we can alias to a static method
+  class << self
+
+    #ruby -r "./TimeWizard.rb" -e "runTimeWizard('Bool.java')"
+    def run(file, days = "01", months = "09")
+
+      time = TimeWizard.new(
+        month = months.to_s,
+        day = days.to_s,
+        hours(),
+        minutes(),
+        seconds()
+      )
+
+      time.teleport_file("#{file}")
+    end
+
+    def minutes
+      (rand(58) + 1).to_s
+    end
+
+    def hours
+      (rand(22) + 1).to_s
+    end
+
+    alias :seconds :minutes
+
+  end
+end
